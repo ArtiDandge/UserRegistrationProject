eb231ac21942b7e5d89327321ca9074eef04e09f
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-19 08:47:33 +0530

    Validate User email

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -12,6 +12,17 @@ public class UserRegistration {
         }
 
     }
+    public static boolean validateEmail(String mail){
+        String emailExp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z.-]+$";
+
+        if (mail.matches(emailExp)){
+            System.out.println("'"+mail+"'"+" validation success !");
+            return true;
+        }else {
+            System.out.println("'"+mail+"'"+" validation success !");
+            return false;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         Scanner sc = new Scanner(System.in);
@@ -21,5 +32,8 @@ public class UserRegistration {
         System.out.println("Enter Last name ");
         String last_name = sc.nextLine();
         System.out.println(validateFirstName(last_name));
+        System.out.println("Enter Mail address");
+        String email = sc.nextLine();
+        System.out.println(validateEmail(email));
     }
 }
